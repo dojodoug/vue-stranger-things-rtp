dd2b13a279d2f47a03415872b9c027a5dd4ab5f6
Doug Bryant <douglas.e.bryant@gmail.com>
2019-09-10 10:06:32 -0400

    Cleaning up eslint warnings

--- stranger-things-rtp/src/components/STNav.vue
+++ stranger-things-rtp/src/components/STNav.vue
@@ -1,6 +1,10 @@
 <template>
   <Slide>
-    <a id="inspiration" href="#inspiration-div" v-scroll-to="'#inspiration-div'">
+    <a
+      id="inspiration"
+      href="#inspiration-div"
+      v-scroll-to="'#inspiration-div'"
+    >
       <span>INSPIRATION</span>
     </a>
     <a id="gallery" href="#gallery-div">
@@ -13,22 +17,22 @@
 </template>
 
 <script>
-  import { Slide } from 'vue-burger-menu'
-  export default {
-    name: "STNav",
-    components: {
-      Slide
-    }
-  };
+import { Slide } from "vue-burger-menu";
+export default {
+  name: "STNav",
+  components: {
+    Slide
+  }
+};
 </script>
 
 <style>
-  .bm-burger-button {
-    top: 15px;
-    left: 20px;
-  }
+.bm-burger-button {
+  top: 15px;
+  left: 20px;
+}
 
-  .bm-burger-bars {
-    background-color: #fff;
-  }
+.bm-burger-bars {
+  background-color: #fff;
+}
 </style>
